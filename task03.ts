c88022d767f3d8caa7f2030ab8893705f9ffdd90
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-11-16 21:06:29 +0700

    change JS to TS : TODO-03

--- task03.ts
+++ task03.ts
@@ -1,6 +1,4 @@
-"use strict";
-
-// TODO: Write if/else if to print:
+// TODO-03: Write if/else if to print:
 // Password < 6 chars → "Too short"
 // 6–10 chars → "Moderate"
 // 11+ chars → "Strong"
